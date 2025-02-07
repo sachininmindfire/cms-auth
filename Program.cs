8f35742aae4e1b80d04a078ce212aec96b88495d
Sachin Kumar <sachininmindfire@gmail.com>
2025-02-07 15:20:33 +0530

    Azure Keyvault integration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using CMS.Auth.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -6,6 +7,19 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.EnvironmentName == "Production")
+{
+    // Add Azure Key Vault
+    var keyVaultEndpoint = new Uri(builder.Configuration["KeyVault:Endpoint"]);
+    builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+}
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Production.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -21,9 +35,20 @@ builder.Services.AddCors(options =>
         });
 });
 
+var dbServer = builder.Configuration["CMSAuth-DbServer"];
+var dbName = builder.Configuration["CMSAuth-DbName"];
+var dbUsername = builder.Configuration["CMSAuth-DbUserName"];
+var dbPassword = builder.Configuration["CMSAuth-DbPassword"];
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    .Replace("{DbServer}", dbServer)
+    .Replace("{DbName}", dbName)
+    .Replace("{DbUsername}", dbUsername)
+    .Replace("{DbPassword}", dbPassword);
+
 // Configure Entity Framework and Identity
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 {
